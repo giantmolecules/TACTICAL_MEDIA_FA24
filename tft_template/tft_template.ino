b852ff21927d34c1ee3d8e219cf5e211184385e8
Brett Ian Balogh <giantmolecules@gmail.com>
2024-10-10 08:41:01 -0500

    Create tft_template.ino

---
+++ tft_template/tft_template.ino
@@ -0,0 +1,61 @@
+//----------------------------------------------------------------//
+//
+// SAIC Tactical Media Fall 2024
+// Brett Ian Balogh
+// https://github.com/giantmolecules/TACTICAL_MEDIA_FA24
+//
+// tft_template.ino
+//
+// This is a template for our code that readies the TFT for use.
+//
+//----------------------------------------------------------------//
+
+// Include libraries for TFT
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+
+// Create a TFT object
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+//----{SETUP}-----------------------------------------------------//
+
+void setup() {
+
+  // Start Serial COM
+  Serial.begin(115200);
+  
+  // turn on backlite
+  pinMode(TFT_BACKLITE, OUTPUT);
+  digitalWrite(TFT_BACKLITE, HIGH);
+
+  // turn on the TFT / I2C power supply
+  pinMode(TFT_I2C_POWER, OUTPUT);
+  digitalWrite(TFT_I2C_POWER, HIGH);
+  delay(10);
+
+  // initialize TFT
+  tft.init(135, 240); // Init ST7789 240x135
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  // Turn off word wrapping
+  tft.setTextWrap(false);
+
+  // default text size
+  tft.setTextSize(4);
+  
+  // set text foreground and background colors
+  tft.setTextColor(ST77XX_WHITE,ST77XX_BLACK);
+
+  Serial.println(F("TFT Initialized"));
+
+}
+
+//----{LOOP}------------------------------------------------------//
+
+void loop() {
+ 
+}
+
+//----{END}------------------------------------------------------//
